76667daf9cf1993a2c12e96bf2672f71dafa86a4
Zhengheng Li <zhenghenge@gmail.com>
2018-07-20 00:26:26 +0800

    script1.py can work and passed test online

--- script1.py
+++ script1.py
@@ -16,7 +16,7 @@ parser.add_argument("--passwd", dest = "mysql_passwd", help = "mysql_passwd", de
 parser.add_argument("--dbname", dest = "mysql_dbname", help = "mysql_dbname", default = "hxmt")
 args = parser.parse_args()
 
-def leap_second(gps_second):
+def leap_second(gps_time):
     if gps_time < 1025136015:
         return 15
     elif gps_time < 1119744016:
@@ -35,6 +35,10 @@ def hxmt_met_to_utc_str(hxmt_met):
     gps_second = 1009411215.0 + hxmt_met
     return gps_to_utc_str(gps_second)
 
+now_datetime = datetime.now()
+now_time_str = now_datetime.strftime('%m/%d/%Y %H:%M:%S')
+now_date_str = now_datetime.strftime('%Y%m%d')
+
 # read owner_dict
 owner_dict = {}
 with open(args.owner_file, 'r') as fin:
@@ -45,7 +49,10 @@ with open(args.owner_file, 'r') as fin:
 # read obs_id_list
 obs_id_list = []
 for line in open(args.obs_id_file, 'r'):
-    obs_id_list.append(line.strip())
+    l = line.strip()
+    if not l: continue
+    if l[0] == '#': continue
+    obs_id_list.append(l)
 
 if not obs_id_list:
     print 'obs_id_list is empty'
@@ -55,24 +62,61 @@ if not obs_id_list:
 where_clause = ''
 for idstr in obs_id_list:
     if not where_clause:
-        where_clause = 'thing = "%s"' % idstr
+        where_clause = 'obs_id = "%s"' % idstr
     else:
-        where_clause += ' or thing = "%s"' % idstr
+        where_clause += ' or obs_id = "%s"' % idstr
+
+obs_id_found_list = []
+obs_id_without_owner_list = []
 
 # read_database
-# TODO: change the headers
-header = ['thing', 'legs', 'arms']
+header = ['数据级别', '标示', '名字', '生成时间', '观测起始时间',
+        '观测起始时间2', '观测结束时间', '观测结束时间2', '是否公开',
+        '数据归属', '时长', '观测号', '目标名', '源坐标', '提案id', '创建人',
+        '是否偏轴', '扫描半径', '扫描间隔', '扫描速度']
+name_dict = {'Q': '小天区观测', 'L': '定点观测', 'Z': '巡天观测'}
 output_rows = []
 try:
     con = mdb.connect(args.mysql_host, args.mysql_user, args.mysql_passwd, args.mysql_dbname)
     cur = con.cursor(mdb.cursors.DictCursor)
-    cur.execute('select * from limbs where ' + where_clause)
+    cur.execute('select * from taskmanager_obs_list_store where ' + where_clause)
     for row in cur.fetchall():
-        # TODO: generate and append rows in output_rows
-        thing = row['thing']
-        legs = str(row['legs'])
-        arms = str(row['arms'])
-        orow_list = [thing, legs, arms]
+        # fetch fields
+        obs_id               = row['obs_id']
+        obs_mode             = row['obs_mode']
+        ensured_start_time   = row['ensured_start_time']
+        ensured_end_time     = row['ensured_end_time']
+        target               = row['target']
+        ra_t                 = row['ra_t']
+        dec_t                = row['dec_t']
+        proposal             = row['proposal']
+        pi                   = row['pi']
+        offset_flag          = row['offset_flag']
+        radius               = row['radius']
+        interval             = row['interval']
+        speed                = row['speed']
+        # check
+        obs_id_found_list.append(obs_id)
+        owner = ''
+        if owner_dict.has_key(proposal):
+            owner = owner_dict[proposal]
+        else:
+            obs_id_without_owner_list.append([obs_id, proposal])
+        # do extra calculations
+        marking = obs_id + '_' + now_date_str
+        obs_mode_name = name_dict[obs_mode]
+        data_level = '1L'
+        gen_time = now_time_str
+        start_time_utc = hxmt_met_to_utc_str(ensured_start_time)
+        end_time_utc = hxmt_met_to_utc_str(ensured_end_time)
+        open_flag = 0
+        obs_duration = ensured_end_time - ensured_start_time
+        target_pos = ', '.join([str(ra_t), str(dec_t)])
+
+        orow_list = [data_level, marking, obs_mode_name, gen_time, start_time_utc,
+                ensured_start_time, end_time_utc, ensured_end_time, open_flag,
+                owner, obs_duration, obs_id, target, target_pos, proposal, pi,
+                offset_flag, radius, interval, speed]
         output_rows.append(orow_list)
 
 except mdb.Error, e:
@@ -85,16 +129,34 @@ if not output_rows:
     print 'found nothing from database'
     exit(1)
 
+obs_id_not_found_list = []
+for x in obs_id_list:
+    if x not in obs_id_found_list:
+        obs_id_not_found_list.append(x)
+if obs_id_not_found_list:
+    print "Warning, the following obs_id are not found in the database:"
+    print '-' * 20
+    for x in obs_id_not_found_list:
+        print x
+    print '-' * 20
+
+if obs_id_without_owner_list:
+    print "Warning, the following obs_id with proposal has no owner found:"
+    print '-' * 20
+    for x in obs_id_without_owner_list:
+        print ', '.join(x)
+    print '-' * 20
+
 # write data
 with open(args.outfile, 'w') as fout:
-    csv_writer = csv.writer(fout, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    csv_writer = csv.writer(fout, delimiter=',', quotechar='"')
     # write header
-    header += ['id']
-    print ', '.join(header)
+    header = ['id'] + header
+    # print ', '.join(header)
     csv_writer.writerow(header)
     # write rows
     for (idx, row) in enumerate(output_rows, start = 1):
-        orow = [str(idx)] + row
-        print ', '.join(orow)
+        orow = [str(x) for x in [idx] + row]
+        # print ', '.join(orow)
         csv_writer.writerow(orow)
 
