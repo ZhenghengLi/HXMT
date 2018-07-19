aed587ad036495f956a4813e11940b194339d970
Zhengheng Li <zhenghenge@gmail.com>
2018-07-20 01:06:57 +0800

    script1.py: shorten the try block

--- script1.py
+++ script1.py
@@ -67,69 +67,74 @@ for idstr in obs_id_list:
     else:
         where_clause += ' or obs_id = "%s"' % idstr
 
-obs_id_found_list = []
-obs_id_without_owner_list = []
-
-# read_database
-header = ['数据级别', '标示', '名字', '生成时间', '观测起始时间',
-        '观测起始时间2', '观测结束时间', '观测结束时间2', '是否公开',
-        '数据归属', '时长', '观测号', '目标名', '源坐标', '提案id', '创建人',
-        '是否偏轴', '扫描半径', '扫描间隔', '扫描速度']
-name_dict = {'Q': '小天区观测', 'L': '定点观测', 'Z': '巡天观测'}
-output_rows = []
+# read database
+all_db_rows = None
 try:
     con = mdb.connect(args.mysql_host, args.mysql_user, args.mysql_passwd, args.mysql_dbname)
     cur = con.cursor(mdb.cursors.DictCursor)
     cur.execute('select * from taskmanager_obs_list_store where ' + where_clause)
-    for row in cur.fetchall():
-        # fetch fields
-        obs_id               = row['obs_id']
-        obs_mode             = row['obs_mode']
-        ensured_start_time   = row['ensured_start_time']
-        ensured_end_time     = row['ensured_end_time']
-        target               = row['target']
-        ra_t                 = row['ra_t']
-        dec_t                = row['dec_t']
-        proposal             = row['proposal']
-        pi                   = row['pi']
-        offset_flag          = row['offset_flag']
-        radius               = row['radius']
-        interval             = row['interval']
-        speed                = row['speed']
-        # check
-        obs_id_found_list.append(obs_id)
-        owner = ''
-        if owner_dict.has_key(proposal):
-            owner = owner_dict[proposal]
-        else:
-            obs_id_without_owner_list.append([obs_id, proposal])
-        # do extra calculations
-        marking = obs_id + '_' + now_date_str
-        obs_mode_name = name_dict[obs_mode]
-        data_level = '1L'
-        gen_time = now_time_str
-        start_time_utc = hxmt_met_to_utc_str(ensured_start_time)
-        end_time_utc = hxmt_met_to_utc_str(ensured_end_time)
-        open_flag = 0
-        obs_duration = ensured_end_time - ensured_start_time
-        target_pos = ', '.join([str(ra_t), str(dec_t)])
-
-        orow_list = [data_level, marking, obs_mode_name, gen_time, start_time_utc,
-                ensured_start_time, end_time_utc, ensured_end_time, open_flag,
-                owner, obs_duration, obs_id, target, target_pos, proposal, pi,
-                offset_flag, radius, interval, speed]
-        output_rows.append(orow_list)
-
+    all_db_rows = cur.fetchall()
 except mdb.Error, e:
     print "MySQL Error %d: %s" % (e.args[0],e.args[1])
     exit(1)
 finally:
     if con: con.close()
 
-if not output_rows:
+if not all_db_rows:
     print 'found nothing from database'
     exit(1)
 
+# process data
+obs_id_found_list = []
+obs_id_without_owner_list = []
+output_rows = []
+
+header = ['数据级别', '标示', '名字', '生成时间', '观测起始时间',
+        '观测起始时间2', '观测结束时间', '观测结束时间2', '是否公开',
+        '数据归属', '时长', '观测号', '目标名', '源坐标', '提案id', '创建人',
+        '是否偏轴', '扫描半径', '扫描间隔', '扫描速度']
+name_dict = {'Q': '小天区观测', 'L': '定点观测', 'Z': '巡天观测'}
+
+for row in all_db_rows:
+    # fetch fields
+    obs_id               = row['obs_id']
+    obs_mode             = row['obs_mode']
+    ensured_start_time   = row['ensured_start_time']
+    ensured_end_time     = row['ensured_end_time']
+    target               = row['target']
+    ra_t                 = row['ra_t']
+    dec_t                = row['dec_t']
+    proposal             = row['proposal']
+    pi                   = row['pi']
+    offset_flag          = row['offset_flag']
+    radius               = row['radius']
+    interval             = row['interval']
+    speed                = row['speed']
+    # check
+    obs_id_found_list.append(obs_id)
+    owner = ''
+    if owner_dict.has_key(proposal):
+        owner = owner_dict[proposal]
+    else:
+        obs_id_without_owner_list.append([obs_id, proposal])
+    # do extra calculations
+    marking = obs_id + '_' + now_date_str
+    obs_mode_name = name_dict[obs_mode]
+    data_level = '1L'
+    gen_time = now_time_str
+    start_time_utc = hxmt_met_to_utc_str(ensured_start_time)
+    end_time_utc = hxmt_met_to_utc_str(ensured_end_time)
+    open_flag = 0
+    obs_duration = ensured_end_time - ensured_start_time
+    target_pos = ', '.join([str(ra_t), str(dec_t)])
+    # generate and append record
+    orow_list = [data_level, marking, obs_mode_name, gen_time, start_time_utc,
+            ensured_start_time, end_time_utc, ensured_end_time, open_flag,
+            owner, obs_duration, obs_id, target, target_pos, proposal, pi,
+            offset_flag, radius, interval, speed]
+    output_rows.append(orow_list)
+
+# check not found obs_id
 obs_id_not_found_list = []
 for x in obs_id_list:
     if x not in obs_id_found_list:
@@ -141,6 +146,7 @@ if obs_id_not_found_list:
         print x
     print '-' * 20
 
+# check not found owner
 if obs_id_without_owner_list:
     print "Warning, the following obs_id with proposal has no owner found:"
     print '-' * 20
@@ -148,7 +154,7 @@ if obs_id_without_owner_list:
         print ', '.join(x)
     print '-' * 20
 
-# write data
+# write data to CSV file
 with open(args.outfile, 'w') as fout:
     csv_writer = csv.writer(fout, delimiter=',', quotechar='"')
     # write header
